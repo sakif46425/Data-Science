bf3bff9906b2dbda6b7f751d4fdcf378edab3010
Sakif Khan <sakifkhan1340@gmail.com>
2025-07-29 14:25:58 +0600

    Add lab-3 file

---
+++ DS lab-3.R
@@ -0,0 +1,24 @@
+var1 = readline(prompt = "Enter any Number: ")
+var2 = readline(prompt = "Enter any Number: ")
+
+var2 = as.integer(var2)
+print(var1)
+print(var2)
+
+
+var1 = readline(prompt = "Enter any number : ")
+var2 = readline(prompt = "Enter any number : ")
+
+var1 = as.integer(var1)
+var2 = as.integer(var2)
+print(var1 + var2)
+
+
+x = scan()
+print(x)
+
+mydata <- data.frame(age = numeric (0),
+gender = character(0),
+weight = numeric(0))
+
+mydata <- edit(mydata)
